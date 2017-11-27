117d4aa7f6d5a4ce50ff59402068c1cc489259e0
Purplexionist <eggertwilliamr@gmail.com>
2017-11-27 00:11:42 -0800

    almost finished guest

--- InnReservations.java
+++ InnReservations.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 import java.sql.*;
 import java.lang.*;
+import java.text.*;
+import java.util.Date;
 
 public class InnReservations {
 	   static String url = null;
@@ -15,8 +17,9 @@ public class InnReservations {
       static String cur = "";
       static int state = 1;
       static int adminState;
+      static int guestState;
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ParseException {
 		
       
       //reads server settings
@@ -102,6 +105,7 @@ public class InnReservations {
       //main loop
       while(state > 0) {
             adminState = 1;
+            guestState = 1;
             System.out.println("Enter A for Admin");
             System.out.println("Enter O for Owner");
             System.out.println("Enter G for Guest");
@@ -112,6 +116,8 @@ public class InnReservations {
                state = 0;
             else if(s.equals("A"))
                runAdmin();
+            else if(s.equals("G"))
+               runGuestMenu();
       }
          
       //closes connection
@@ -177,6 +183,7 @@ public class InnReservations {
       else {
            System.out.println("Enter Ro for rooms");
            System.out.println("Enter Re for reservations");
+           System.out.println("Enter anything else to return to admin display");
            Scanner scan = new Scanner(System.in);
            String str = scan.next();
            ResultSet rs;
@@ -315,6 +322,244 @@ public class InnReservations {
       reservationsFilled = false;
       roomsExist = false;
       roomsFilled = false;
+      }
+      catch(Exception e) { System.out.println(e); }
+   }
+   
+   public static void runGuestMenu() throws ParseException {
+      while(guestState > 0) {
+         System.out.println("");
+         System.out.println("Enter Ro for rooms and rates");
+         System.out.println("Enter Re for reservations");
+         System.out.println("Enter E to return to switch user type");
+         Scanner slay = new Scanner(System.in);
+         String na = slay.next();
+         System.out.println("");
+         if(na.equals("E"))
+            guestState = 0;
+         else if(na.equals("Ro"))
+            runGuestRooms();
+         else if(na.equals("Re")) {
+            if(roomsFilled && roomsExist)
+               runGuestRes();
+            else
+               System.out.println("Tables not created/filled");
+         }
+      }
+   }
+   
+   public static void runGuestRooms() {
+      ResultSet rs;
+      PreparedStatement statement;
+      if(roomsExist && roomsFilled) {
+         try {
+            statement = conn.prepareStatement("select * from rooms");
+            rs = statement.executeQuery();
+            int i = 1;
+            while(rs.next()) {
+                    System.out.print(i+"  ");
+                    System.out.println(rs.getString("RoomCode")+"  ");
+                    i++;
+            }
+         }
+      catch(Exception e){ System.out.println(e); }
+      System.out.println("Choose a room by entering its number");
+      Scanner s = new Scanner(System.in);
+      String room = s.next();
+      try {
+         statement = conn.prepareStatement("select * from rooms");
+         rs = statement.executeQuery();
+         int roomNum = Integer.parseInt(room);
+         if(roomNum < 11 && roomNum > 0 && rs != null) {
+            rs.absolute(roomNum);
+            System.out.println("");
+            System.out.println("RoomCode  RoomName  Beds  bedType  Occupancy  Price  Decor");
+            System.out.print(rs.getString("RoomCode")+"  ");
+            System.out.print(rs.getString("RoomName")+"  ");
+            System.out.print(rs.getInt("Beds")+"  ");
+            System.out.print(rs.getString("bedType")+"  ");
+            System.out.print(rs.getInt("maxOcc")+"  ");
+            System.out.print(rs.getFloat("basePrice")+"  ");
+            System.out.print(rs.getString("decor")+"  ");
+            System.out.println("\n");
+            System.out.println("Enter C to check availability");
+            System.out.println("Enter anything else to return to guest menu");
+            room = s.next();
+            if(room.equals("C"))
+               checkAvai(roomNum, rs.getString("RoomCode"),  rs.getFloat("basePrice"), rs.getString("RoomName"));
+         }
+      }
+      catch(Exception e){System.out.println("Bad input");}
+      }
+      else
+         System.out.println("Tables not created/filled");
+   }
+   
+   public static void checkAvai(int roomNum, String roomCode, double bP, String roomName) throws ParseException {
+      boolean occupied = false;
+      System.out.println("\nEnter the CheckIn date in YYYY DD MM format");
+      Scanner scan = new Scanner(System.in);
+      String t = scan.nextLine();
+      StringTokenizer st = new StringTokenizer(t);
+      String checkIn = "";
+      checkIn = checkIn + st.nextToken()+"."+st.nextToken()+"."+st.nextToken();
+      System.out.println("\nEnter the CheckOut date in YYYY DD MM format");
+      t = scan.nextLine();
+      st = new StringTokenizer(t);
+      String checkOut = "";
+      checkOut = checkOut + st.nextToken()+"."+st.nextToken()+"."+st.nextToken();
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy.dd.MM");
+      SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+      Date starts = sdf.parse(checkIn);
+      Date ends = sdf.parse(checkOut);
+      double mult = 1.0;
+      Date j1 = sdf.parse("2010.01.01");
+      Date j4 = sdf.parse("2010.07.04");
+      Date s6 = sdf.parse("2010.09.06");
+      Date o3 = sdf.parse("2010.10.30");
+      Date jj = sdf.parse("2011.01.01");
+      Calendar start = Calendar.getInstance();
+      start.setTime(starts);
+      Calendar end = Calendar.getInstance();
+      end.setTime(ends);
+      if((j1.compareTo(starts) >= 0 && j1.compareTo(ends) < 0) ||
+         (j4.compareTo(starts) >= 0 && j4.compareTo(ends) < 0) ||
+         (s6.compareTo(starts) >= 0 && s6.compareTo(ends) < 0) ||
+         (o3.compareTo(starts) >= 0 && o3.compareTo(ends) < 0) ||
+         (jj.compareTo(starts) >= 0 && jj.compareTo(ends) < 0))
+         mult = 1.25;
+      if(mult == 1.0) {
+         for(Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
+            if(start.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
+               start.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
+               mult = 1.1;
+         }
+      }
+      start.setTime(starts);
+      for(Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
+         System.out.println("");
+         System.out.printf(sdf2.format(date));
+         try {
+            Statement s = conn.createStatement();
+            ResultSet rs;
+            rs = s.executeQuery("SELECT COUNT(*) FROM reservations WHERE CheckIn <= '" +sdf2.format(date)+
+            "' && Checkout > '"+sdf2.format(date)+"' && Room = '"+roomCode+"'");
+            rs.next();
+            if(!(rs.getInt(1) > 0))
+               System.out.print(" "+bP*mult);
+            else {
+               occupied = true;
+               System.out.print(" Occupied");
+            }
+         }
+         catch(Exception e){ System.out.println(e); }
+      }
+      if(!occupied) {
+         System.out.println("\nEnter P to place a reservation");
+         System.out.println("Enter anything else to return to guest menu");
+         Scanner newNew = new Scanner(System.in);
+         if(newNew.next().equals("P"))
+            System.out.println("place a res");
+            //completeReservation(starts, ends, bP*mult, roomCode, roomName);
+      }
+   }
+   
+   public static void runGuestRes() throws ParseException {
+      try {
+         System.out.println("\nEnter the CheckIn date in YYYY DD MM format");
+         Scanner scan = new Scanner(System.in);
+         String t = scan.nextLine();
+         StringTokenizer st = new StringTokenizer(t);
+         String checkIn = "";
+         checkIn = checkIn + st.nextToken()+"."+st.nextToken()+"."+st.nextToken();
+         System.out.println("\nEnter the CheckOut date in YYYY DD MM format");
+         t = scan.nextLine();
+         st = new StringTokenizer(t);
+         String checkOut = "";
+         checkOut = checkOut + st.nextToken()+"."+st.nextToken()+"."+st.nextToken();
+         SimpleDateFormat sdf = new SimpleDateFormat("yyyy.dd.MM");
+         SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+         Date starts = sdf.parse(checkIn);
+         Date ends = sdf.parse(checkOut);
+         double mult = 1.0;
+         Date j1 = sdf.parse("2010.01.01");
+         Date j4 = sdf.parse("2010.07.04");
+         Date s6 = sdf.parse("2010.09.06");
+         Date o3 = sdf.parse("2010.10.30");
+         Date jj = sdf.parse("2011.01.01");
+         Calendar start = Calendar.getInstance();
+         start.setTime(starts);
+         Calendar end = Calendar.getInstance();
+         end.setTime(ends);
+         if((j1.compareTo(starts) >= 0 && j1.compareTo(ends) < 0) ||
+            (j4.compareTo(starts) >= 0 && j4.compareTo(ends) < 0) ||
+            (s6.compareTo(starts) >= 0 && s6.compareTo(ends) < 0) ||
+            (o3.compareTo(starts) >= 0 && o3.compareTo(ends) < 0) ||
+            (jj.compareTo(starts) >= 0 && jj.compareTo(ends) < 0))
+            mult = 1.25;
+         if(mult == 1.0) {
+            for(Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
+               if(start.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
+                  start.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
+                  mult = 1.1;
+            }
+         }
+         PreparedStatement s = conn.prepareStatement("SELECT RoomCode FROM rooms");
+         ResultSet rs = s.executeQuery();
+         ArrayList<String> validRooms = new ArrayList<String>();
+         while(rs.next()) {
+            validRooms.add(rs.getString("RoomCode"));
+         }
+         start.setTime(starts);
+         Statement sa = conn.createStatement();
+         ResultSet rsa;
+         for(Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
+            try {
+               rsa = sa.executeQuery("SELECT DISTINCT Room FROM reservations WHERE CheckIn <= '" +sdf2.format(date)+
+               "' && Checkout > '"+sdf2.format(date)+"'");
+               while(rsa.next()) {
+                  if(validRooms.contains(rsa.getString("Room")))
+                     validRooms.remove(rsa.getString("Room"));
+               }
+            }
+            catch(Exception e){ System.out.println(e); }
+         }
+         ArrayList<String> realName = new ArrayList<String>();
+         ArrayList<Float> prices = new ArrayList<Float>();
+         for(int i = 0; i < validRooms.size(); i++) {
+            s = conn.prepareStatement("SELECT RoomName, basePrice FROM rooms WHERE RoomCode = '"+validRooms.get(i)+"'");
+            rs = s.executeQuery();
+            rs.next();
+            realName.add(rs.getString("RoomName"));
+            prices.add(rs.getFloat("basePrice")*(float)mult);
+         }
+         for(int i = 0; i < validRooms.size(); i++) {
+            System.out.print(i+1+"\t");
+            System.out.print(validRooms.get(i)+"\t");
+            System.out.print(realName.get(i)+"\t");
+            System.out.print(prices.get(i)+"\n");
+         }
+         System.out.println("Enter the room number you would like to view");
+         System.out.println("Enter anything else to return to guest menu");
+         Scanner sce = new Scanner(System.in);
+         int sca = sce.nextInt();
+         if(sca > 0 && sca < validRooms.size()) {
+            s = conn.prepareStatement("SELECT * FROM rooms WHERE RoomCode = '"+validRooms.get(sca-1)+"'");
+            rs = s.executeQuery();
+            rs.next();
+            System.out.println("RoomCode  RoomName  Beds  bedType  Occupancy  Price  Decor");
+            System.out.print(rs.getString("RoomCode")+"  ");
+            System.out.print(rs.getString("RoomName")+"  ");
+            System.out.print(rs.getInt("Beds")+"  ");
+            System.out.print(rs.getString("bedType")+"  ");
+            System.out.print(rs.getInt("maxOcc")+"  ");
+            System.out.print(rs.getFloat("basePrice")+"  ");
+            System.out.print(rs.getString("decor")+"  ");
+            System.out.println("\nEnter P to place a reservation\nEnter anything else to return to guest menu");
+            if(sce.next().equals("P"))
+               //completeReservation(starts, ends, rs.getFloat("basePrice")*(float)mult,
+            //rs.getString("RoomCode"), rs.getString("RoomName"));
+         }
       }
       catch(Exception e) { System.out.println(e); }
    }
